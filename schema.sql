a0bbb7557375c5234d1b7362facf68c9807a4b59
Vesa <vesainwonderland@gmail.com>
2020-09-20 13:23:49 +0300

    add schema.sql

---
+++ schema.sql
@@ -0,0 +1,3 @@
+CREATE TABLE books (id SERIAL PRIMARY KEY, name TEXT, lenght INTEGER, publication_date INTEGER, author TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT, password TEXT);
+CREATE TABLE rewiews (id SERIAL PRIMARY KEY, text TEXT, book_id INTEGER REFERENCES books, time TIMESTAMP DEFAULT NOW(), score INTEGER);
